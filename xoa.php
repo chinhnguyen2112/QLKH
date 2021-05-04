45f3a79de67aff447b7af1d9b7a76f3ccbdeff7e
chinhnguyen2112 <79840129+chinhnguyen2112@users.noreply.github.com>
2021-05-05 04:42:32 +0700

    Update xoa.php
    yess

--- xoa.php
+++ xoa.php
@@ -1,36 +1,14 @@
-
-<form action="#" method="GET">
-    <table border="10">
-    <?php
-include('connect.php');
-echo '<select name="MaKH">';
-echo '<option value="">-----Chọn---</option>';
-            $xoa=mysqli_query($connect,"SELECT  * FROM muahang");
-            while($show=mysqli_fetch_assoc($xoa))
-            {
-                echo '<option value="' . htmlspecialchars($show["MaKH"]) . '">'  . htmlspecialchars($show["MaKH"]).'</option>';
-            }
-            '</select>'
-        
-?>
-           
-          <td><input type="submit" name="xoa" value="XÓA"></td>||<td><a href="index.php">««Back </a></td>
-    
-        
-    </table>
-
-</form>
 <?php
+        
+            
+        
+$id= $_GET['id'];
+include('connect.php');
 
-    if(isset($_GET['xoa']))
-    {
-        $ma=$_GET['MaKH'];
-       
-        $query=mysqli_query($connect,"DELETE FROM `muahang` WHERE MaKH=$ma");
-        if($query){
-            echo "<script> alert('Xóa thành công')</script>";
-        }
-        else echo  "<script> alert('Xóa thất bại')</script>";
-    }
+
+$query = mysqli_query($connect,"DELETE  FROM `muahang` where MaKH='$id'") or die ("ERRRO");
+
+
+header("location:index.php");
+
 ?>
-
