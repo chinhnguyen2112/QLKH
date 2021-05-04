28f5179a6edd6121c032f1c3c388c05183878302
chinhnguyen2112 <79840129+chinhnguyen2112@users.noreply.github.com>
2021-05-05 00:45:55 +0700

    Update sua1.php

--- sua1.php
+++ sua1.php
@@ -4,12 +4,12 @@ $ma=$_POST['ma'];
 $query=mysqli_query($connect,"SELECT* FROM muahang where MaKH=$ma");
 $row=mysqli_fetch_assoc($query);
 $makh=$row['MaKH'];
-$ten=$row['TenKH'];
+$ten=$row['HoTen'];
 $sl=$row['SoLuong'];
 $gia=$row['DonGia'];
 
 ?>
-<form action="#" method="POST">
+<form action="index.php" method="POST">
     <table border="2">
         <tr>
             <td>Mã KH</td>
@@ -44,6 +44,10 @@ $gia=$row['DonGia'];
         $ten=$_POST['ten'];
         $gia=$_POST['gia'];
         $sl=$_POST['sl'];
-        $show=mysqli_query($connect,"UPDATE `muahang` SET `MaKH`='$makh',`TenKH`='$ten',`SoLuong`='$sl',`DonGia`='$gia' WHERE 1");
+        $show=mysqli_query($connect,"UPDATE `muahang` SET `MaKH`='$makh',`HoTen`='$ten',`SoLuong`='$sl',`DonGia`='$gia' WHERE 1");
+        if($show){
+            echo "<script> alert('Sửa thành công')</script>";
+        }
+        else echo  "<script> alert('Sửa thất bại')</script>";
     }
 ?>
