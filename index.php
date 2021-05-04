8cdd88986a78819b79fbe1ef3c553bf5e397ac63
chinhnguyen2112 <79840129+chinhnguyen2112@users.noreply.github.com>
2021-05-05 03:32:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,49 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Khách hàng</title>
+<link type="text/css" href="style.css" rel="stylesheet" media="screen">
+</head>
+<body>
+
 <?php
     include('connect.php');
 ?>
-<form action="#" method="POST">
-    <table border="10">
-        <tr>
-            <td>Mã KH</td>
-            <td>Tên kh</td>
-            <td>Số lượnbg</td>
-            <td>Đơn giá</td>
-            <td>Tổng tiền</td>
-            <td>Chức năng</td>
-        </tr>
-        <?php
-            $query=mysqli_query($connect,"SELECT * FROM muahang");
-            while($show=mysqli_fetch_assoc($query))
-            {
-                $sum=0;
-        ?>
-        <tr>
-            <td><?php echo $show['MaKH'] ?></td>
-            <td><?php echo $show['HoTen'] ?></td>
-            <td><?php echo $show['SoLuong'] ?></td>
-            <td><?php echo $show['DonGia'] ?></td>
-            <td><?php echo $show['SoLuong']*$show['DonGia'] ?></td>
-            <td>
-                <a href="them.php">Thêm</a> || <a href="sua.php">Sửa</a> || <a href="xoa.php">Xóa</a>
-             </td>
-        </tr>
-        <?php
-                
-            }
-            ?>
-        
-    </table>
-</form>
+<div class="content">
+<h1>Thông tin khách hàng</h1>
+        <form action="#" method="POST">
+
+                <tr>
+                    <td>Mã KH</td>
+                    <td>Tên KH</td>
+                    <td>Số lượng</td>
+                    <td>Đơn giá</td>
+                    <td>Tổng tiền</td>
+                    <td>Chức năng</td>
+                </tr>
+                <?php
+                    $query=mysqli_query($connect,"SELECT * FROM muahang");
+                    while($show=mysqli_fetch_assoc($query))
+                    {
+                        $sum=0;
+                ?>
+                <tr>
+                    <td><?php echo $show['MaKH'] ?></td>
+                    <td><?php echo $show['HoTen'] ?></td>
+                    <td><?php echo $show['SoLuong'] ?></td>
+                    <td><?php echo $show['DonGia'] ?></td>
+                    <td><?php echo $show['SoLuong']*$show['DonGia'] ?></td>
+                    <td>
+                        <a href="them.php">Thêm</a> || <a href="sua.php">Sửa</a> || <a href="xoa.php">Xóa</a>
+                     </td>
+                </tr>
+                <?php
+
+                    }
+                    ?>
+
+
+        </form>
+ </div>
+    </body>
