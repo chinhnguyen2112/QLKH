fe1b5ae2ce8a16e2eda6bb670659a9ee271c5b1a
chinhnguyen2112 <79840129+chinhnguyen2112@users.noreply.github.com>
2021-05-05 02:23:49 +0700

    Update sua1.php

--- sua1.php
+++ sua1.php
@@ -1,13 +1,3 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Sửa thông tin khách hàng</title>
-<link type="text/css" href="style.css" rel="stylesheet" media="screen">
-</head>
-<body>
-
-
 <?php 
 include("connect.php");
 $ma=$_POST['ma'];
@@ -19,36 +9,34 @@ $sl=$row['SoLuong'];
 $gia=$row['DonGia'];
 
 ?>
-<div class="content">
-<h1>Sửa thông tin khách hàng</h1>
 <form action="#" method="POST">
-  
-        <div class="row">
-            <span>Mã KH</span>
-            <input type="text" name="ma" value="<?php echo $makh; ?>">
-       </div>
-       <div class="row">
-            <span>Tên KH</span>
-            <input type="text" name="ten" value="<?php echo $ten; ?>">
-            </div>
+    <table border="2">
+        <tr>
+            <td>Mã KH</td>
+            <td><input type="text" name="ma" value="<?php echo $makh ?>"></td>
+        </tr>
+        <tr>
+            <td>Tên KH</td>
+            <td><input type="text" name="ten" value="<?php echo $ten ?>"></td>
+        </tr>
    
-        <div class="row">
-            <span>Đơn giá</span>
-           <input type="text" name="gia"value="<?php echo $gia; ?>">
-            </div>
+    <tr>
+            <td>Đơn Giá</td>
+            <td><input type="text" name="gia"value="<?php echo $gia ?>"></td>
+        </tr>
        
-        <div class="row">
-            <span>Số lượng</span>
-            <input type="text" name="sl"value="<?php echo $sl; ?>">
-            </div>
-        <div class="row">
-            <input type="submit"  value="SỬA">
-            </div>
+    <tr>
+            <td>Số Lượng</td>
+            <td><input type="text" name="sl"value="<?php echo $sl ?>"></td>
+        </tr>
+        <tr>
+            
+            <td><input type="submit" name="sua" value="SỬA"></td> ||<td><a href="index.php">« Back </a></td>
+        </tr>
     
    
- 
+    </table>
 </form>
-</div>
 <?php
     if(isset($_POST['sua']))
     {
@@ -63,6 +51,3 @@ $gia=$row['DonGia'];
         else echo  "<script> alert('Sửa thất bại')</script>";
     }
 ?>
-
-
-</body>
