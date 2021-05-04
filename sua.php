a2304666cc5f6e8b593df1a4847dabbdc2e9cba1
chinhnguyen2112 <79840129+chinhnguyen2112@users.noreply.github.com>
2021-05-05 01:43:53 +0700

    Update sua.php

--- sua.php
+++ sua.php
@@ -1,9 +1,17 @@
 <form action="sua1.php" method="POST">
     <table border="2">
-        <tr>
-            <td>Mã KH</td>
-            <td><input type="text" name="ma">
-        </tr>
+         <?php
+include('connect.php');
+echo '<select name="MaKH">';
+echo '<option value="">-----Chọn---</option>';
+            $xoa=mysqli_query($connect,"SELECT  * FROM muahang");
+            while($show=mysqli_fetch_assoc($xoa))
+            {
+                echo '<option value="' . htmlspecialchars($show["MaKH"]) . '">'  . htmlspecialchars($show["MaKH"]).'</option>';
+            }
+            '</select>'
+        
+?>
         <tr>
             <td><input type="submit" name="go" value="GO"></td>
         </tr>
